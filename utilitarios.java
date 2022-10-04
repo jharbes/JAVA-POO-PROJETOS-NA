7731ddd2ca32a28bb69acd553f75eb87859add77
Jorge Harbes <jharbes@hotmail.com>
2022-10-04 12:14:41 -0300

    ex03

--- utilitarios.java
+++ utilitarios.java
@@ -2,5 +2,9 @@ utilitarios java
 
 String.format("%.2f",variavel)  - colocar com duas casas decimais***
 
+Locale.setDefault(Locale.US);
+
+Scanner teclado=new Scanner(System.in);
+
 
 teclado.nextLine();  -  nao pular a necessidade de entrada.
