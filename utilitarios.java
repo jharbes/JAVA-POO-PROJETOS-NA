deb0b28959f0eca94605bb80c500fc22816b4338
Jorge Harbes <jharbes@hotmail.com>
2022-10-05 07:48:12 -0300

    Update utilitarios.java

--- utilitarios.java
+++ utilitarios.java
@@ -19,3 +19,24 @@ for (String x:vetor){
 	System.out.println(x);
 }
 -------------------------------
+
+---------------------------------------------------------------
+LISTAS EM JAVA
+
+• Tamanho da lista: size()
+
+• Obter o elemento de uma posição: get(position)
+
+• Inserir elemento na lista: add(obj), add(int, obj)
+
+• Remover elementos da lista: remove(obj), remove(int), removeIf(Predicate)
+	
+• Encontrar posição de elemento: indexOf(obj), lastIndexOf(obj)
+
+• Filtrar lista com base em predicado:
+List<Integer> result = list.stream().filter(x -> x > 4).collect(Collectors.toList());
+
+• Encontrar primeira ocorrência com base em predicado:
+Integer result = list.stream().filter(x -> x > 4).findFirst().orElse(null);
+
+----------------------------------------------------------------
