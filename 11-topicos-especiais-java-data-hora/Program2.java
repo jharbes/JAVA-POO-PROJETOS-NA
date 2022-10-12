d4aca3e3c94c12ba379eccc9c730152d74480783
Jorge Harbes <jharbes@hotmail.com>
2022-10-12 09:36:46 -0300

    Update Program2.java

--- 11-topicos-especiais-java-data-hora/Program2.java
+++ 11-topicos-especiais-java-data-hora/Program2.java
@@ -10,20 +10,21 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
-		LocalDate d04 = LocalDate.parse("2022-07-20");
+		LocalDate d04 = LocalDate.parse("2022-07-20"); // *** OBJETO DE DATA E HORA ATUAL CRIADO
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
 		// https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
 		
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // cria o formato fmt1 no qual a data ficará no formato dd/MM/yyyy OBS1
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // cria o objeto fmt1 no qual a data ficará no formato dd/MM/yyyy OBS1 *** OBJETO COM PADRÃO DE FORMATAÇÂO ***
 		
-		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"); // criando o objeto fmt2 no qual a data e hora ficará com o formato dd/MM/yyyy HH:mm
+		
 		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
 		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
 		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 		
-		System.out.println("d04 = " + d04.format(fmt1)); // imprime a data d04 no formato criado acima fmt1, ficando no formato dd/MM/yyyy OBS1
+		System.out.println("d04 = " + d04.format(fmt1)); // imprime a data d04 no formato criado acima fmt1, ficando no formato dd/MM/yyyy OBS1 *** CONVERSÃO PARA IMPRESSÃO DE UM OBJETO DATA -HORA PARA OUTRO PADRÃO DE IMPRESSAO ESCOLHIDO)
 		
 		System.out.println("d04 = " + fmt1.format(d04)); // mesmo resultado do item anterior, porém a chamada é pelo método do objeto fmt1. OBS1
 		
