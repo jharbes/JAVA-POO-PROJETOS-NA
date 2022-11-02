9100e350c7a6c33a2f4d1b84637e99033fd27816
Jorge Harbes <jharbes@hotmail.com>
2022-11-02 07:00:42 -0300

    finalizacao das classes basicas ex01

--- 14-heranca-polimorfismo/ex01-fixacao-etiquetaDePreco/src/entities/UsedProduct.java
+++ 14-heranca-polimorfismo/ex01-fixacao-etiquetaDePreco/src/entities/UsedProduct.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class UsedProduct extends {
+import java.util.Date;
 
+public class UsedProduct extends Product {
+	
+	private Date manufactureDate;
+	
+	public UsedProduct() {
+		super();
+	}
+
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+
+	@Override
+	public String toString() {
+		return super().toString()+"UsedProduct [manufactureDate=" + manufactureDate + "]";
+	}
+	
 }
