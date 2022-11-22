204254bd440771375c5c67d1971dea18bad8dcd2
Jorge Harbes <jharbes@hotmail.com>
2022-11-22 15:52:19 -0300

    ex01 contaBancaria tratamentoExcecoes commit  02

--- 15-tratamento-de-excecoes/ex01-fixacao-contaBancaria-tratamentoExcecoes/src/application/Program.java
+++ 15-tratamento-de-excecoes/ex01-fixacao-contaBancaria-tratamentoExcecoes/src/application/Program.java
@@ -1,5 +1,45 @@
 package application;
 
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
 public class Program {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Enter account data");
+		
+		try {
+			System.out.print("Number: ");
+			Integer number = teclado.nextInt();
 	
+			System.out.print("Holder: ");
+			teclado.nextLine();
+			String holder = teclado.nextLine();
+	
+			System.out.print("Initial balance: ");
+			Double initialBalance = teclado.nextDouble();
+	
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = teclado.nextDouble();
+	
+			System.out.print("\nEnter amount for withdraw: ");
+			Double withdrawAmount = teclado.nextDouble();
+			
+			Account c1=new Account(number, holder, initialBalance, withdrawLimit);
+			
+			c1.deposit(500.0);
+			
+			System.out.println(c1);
+		}
+		catch (Exception e) {
+			System.out.println("Error in accessing account: " + e);
+		}
+		
+		
+		teclado.close();
+	}
 }
--- 15-tratamento-de-excecoes/ex01-fixacao-contaBancaria-tratamentoExcecoes/src/model/entities/Account.java
+++ 15-tratamento-de-excecoes/ex01-fixacao-contaBancaria-tratamentoExcecoes/src/model/entities/Account.java
@@ -1,5 +1,66 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+
+	public Account() {
+
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		super();
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
 	
+	public void deposit(Double deposit) {
+		try {
+		this.balance+=deposit;
+		} catch (Exception e) {
+			System.out.println("Error: "+e);
+		}
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [number=" + number + ", holder=" + holder + ", balance=" + balance + ", withdrawLimit="
+				+ withdrawLimit + "]";
+	}
+
 }
