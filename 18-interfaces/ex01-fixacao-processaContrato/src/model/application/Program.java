da9609b5a7a1ce3e30cc5e44af064409fb43a927
Jorge Harbes <jharbes@hotmail.com>
2023-01-30 10:45:10 -0300

    acerto de impressao de planilha com valores e datas, falta acerto do calculo das demais prestacoes

--- 18-interfaces/ex01-fixacao-processaContrato/src/model/application/Program.java
+++ 18-interfaces/ex01-fixacao-processaContrato/src/model/application/Program.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import model.entities.Contract;
+import model.entities.Installment;
 import model.services.ContractService;
 import model.services.PaypalService;
 
@@ -20,24 +21,24 @@ public class Program {
 		Integer numeroContrato = teclado.nextInt();
 
 		System.out.print("Data (dd/MM/yyyy): ");
-		LocalDate dataContrato = LocalDate.parse(teclado.nextLine(), fmt);
+		LocalDate dataContrato = LocalDate.parse(teclado.next(), fmt);
 
 		System.out.print("Valor do contrato: ");
 		Double valorContrato = teclado.nextDouble();
-		
+
 		System.out.print("Entre com o número de parcelas: ");
 		Integer numeroMesesContrato = teclado.nextInt();
 
 		Contract contrato = new Contract(numeroContrato, dataContrato, valorContrato);
-		
+
 		ContractService contractService = new ContractService(new PaypalService());
-		
+
 		contractService.processContract(contrato, numeroMesesContrato);
 
-		
-
 		System.out.println("Parcelas:");
-		
+
+		for (Installment parcela : contrato.getInstallments())
+			System.out.println(parcela.getDate().format(fmt) + " - " + parcela.getAmount());
 
 		teclado.close();
 	}
