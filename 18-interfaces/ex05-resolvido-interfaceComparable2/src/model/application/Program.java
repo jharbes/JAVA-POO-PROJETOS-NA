e728db7697900a5d590f6ba3a9ea0ae7b9f496dd
Jorge Harbes <jharbes@hotmail.com>
2023-01-31 15:21:26 -0300

    ex05 resolvido interface comparable 2 fim

--- 18-interfaces/ex05-resolvido-interfaceComparable2/src/model/application/Program.java
+++ 18-interfaces/ex05-resolvido-interfaceComparable2/src/model/application/Program.java
@@ -7,22 +7,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import model.entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		List<String> list = new ArrayList<>();
-		String path = "C:\\Users\\Jorge\\Desktop\\Udemy\\JAVA-POO-PROJETOS-NA\\18-interfaces\\names.txt";
+		List<Employee> list = new ArrayList<>();
+		String path = "C:\\Users\\Jorge\\Desktop\\Udemy\\JAVA-POO-PROJETOS-NA\\18-interfaces\\funcionarios.csv";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String employeeCsv = br.readLine();
+			while (employeeCsv != null) {
+				String[] fields = employeeCsv.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employeeCsv = br.readLine();
 			}
+			// para que seja possivel fazer o sort agora que a lista eh do tipo Employee
+			// teremos que implementar a interface comparable
 			Collections.sort(list);
-			for (String s : list) {
+			for (Employee s : list) {
 				System.out.println(s);
 			}
 		} catch (IOException e) {
