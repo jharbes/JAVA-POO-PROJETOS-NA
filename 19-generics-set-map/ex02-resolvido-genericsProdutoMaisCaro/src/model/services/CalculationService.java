09a1017d5306c29921537479e31e41f90a98e154
Jorge Harbes <jharbes@hotmail.com>
2023-02-01 14:22:11 -0300

    inclusao da versao alternativa do metodo max()

--- 19-generics-set-map/ex02-resolvido-genericsProdutoMaisCaro/src/model/services/CalculationService.java
+++ 19-generics-set-map/ex02-resolvido-genericsProdutoMaisCaro/src/model/services/CalculationService.java
@@ -14,8 +14,23 @@ public class CalculationService {
 			if (item.compareTo(max) > 0)
 				max = item;
 		}
-
 		return max;
 	}
+		
+	/*
+		public static <T extends Comparable<? super T>> T max(List<T> list) {
+			if (list.isEmpty()) {
+			throw new IllegalStateException("List can't be empty");
+			}
+			T max = list.get(0);
+			for (T item : list) {
+				if (item.compareTo(max) > 0) {
+					max = item;
+				}
+			} 
+			return max;
+		}
+
+	*/
 
 }
