698a3178b2c48542a5983da33709a7b7e23ed188
Jorge Harbes <jharbes@hotmail.com>
2023-02-01 15:25:14 -0300

    Update utilitarios-spring.java

--- utilitarios-spring.java
+++ utilitarios-spring.java
@@ -18,4 +18,31 @@ Classes que vao receber algum tipo de requisicao
 
 
 @Autowired
-Cria pontos de injecao das classes onde foram utilizados Beans, ou seja, naquele ponto havera injecao das instancias das classes, onde o spring ira criar, manipular e destruir elementos daquela classe quando for preciso.
+Cria pontos de injecao das classes onde foram utilizados Beans, ou seja, naquele ponto havera injecao das instancias das classes, onde o spring ira criar, manipular e destruir elementos daquela classe quando for preciso. Caso seja necessario utilizar alguma classe dentro de outra classe no spring ela sera incluida com o @Autowired
+
+
+
+@Bean
+Utilizado para mostrar ao spring que algum metodo de uma classe nao criada pelo usuario tambem precisa ser controlada pelo spring, tambem podendo criar pontos de injecao dessas em outras classes do programa
+
+
+
+@RestController
+facilita a criacao de APIs REST, aponta a classe como um bean do MVC, indica que suas respostas tem como body um JSON. o Proprio spring fará a conversao do objeto em questao para JSON na hora de devolver o request
+
+
+Repositories
+utilizam a notacao extends JpaRepository, o que desobriga a utilizacao de beans para indicar que ele também sera manipulado pelo spring.
+
+
+
+@RequestMapping exemplo: @RequestMapping("/pessoa")
+indica em qual endPoint os metodos da classe serao executados. um comando post ou get no caminho indicado chamara os metodos definidos em @PostMapping e @GetMapping respectivamente.
+
+
+
+@SpringBootApplication
+Define que aquela sera uma aplicacao spring boot e ja fara a importacao de bibliotecas importantes para o aplicativo
+
+
+*/
