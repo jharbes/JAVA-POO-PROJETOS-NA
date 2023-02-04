cc3722916f67349a02915c2c0473685cd16c657e
Jorge Harbes <jharbes@hotmail.com>
2023-02-04 15:15:12 -0300

    Update Program.java

--- 19-generics-set-map/ex10-resolvido-Set-LinkedHashSet-Demonstracao1/src/model/application/Program.java
+++ 19-generics-set-map/ex10-resolvido-Set-LinkedHashSet-Demonstracao1/src/model/application/Program.java
@@ -12,6 +12,17 @@ public class Program {
 		set.add("TV");
 		set.add("Notebook");
 		set.add("Tablet");
+		set.add("PS5");
+		set.add("Switch");
+		set.add("Xbox");
+
+		set.remove("PS5");
+
+		// remove todos os elementos cujo comprimento seja maior do que sete caracteres
+		set.removeIf(x -> x.length() >= 7);
+
+		// remove todos os elementos cuja primeira letra seja T(case sensitive)
+		set.removeIf(x -> x.charAt(0) == 'T');
 
 		System.out.println(set.contains("Notebook"));
 
