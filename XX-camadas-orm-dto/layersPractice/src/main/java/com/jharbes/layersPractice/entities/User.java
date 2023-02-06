700ff90f0c6e76451075c7737f7635ae59335b49
Jorge Harbes <jharbes@hotmail.com>
2023-02-06 15:24:52 -0300

    criacao do UserDto, UserRepository e UserService

--- XX-camadas-orm-dto/layersPractice/src/main/java/com/jharbes/layersPractice/entities/User.java
+++ XX-camadas-orm-dto/layersPractice/src/main/java/com/jharbes/layersPractice/entities/User.java
@@ -3,14 +3,21 @@ package com.jharbes.layersPractice.entities;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String password;
-	
+
 	public User() {
 	}
 
@@ -66,5 +73,5 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", password=" + password + "]";
 	}
-	
+
 }
