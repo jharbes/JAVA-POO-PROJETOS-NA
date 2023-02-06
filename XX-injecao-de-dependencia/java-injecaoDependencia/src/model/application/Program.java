99f5125ab8298f966e5fc7d6582978be0fc63e9e
Jorge Harbes <jharbes@hotmail.com>
2023-02-06 11:50:28 -0300

    inicio injecao de dependencia em Spring

--- XX-injecao-de-dependencia/java-injecaoDependencia/src/model/application/Program.java
+++ XX-injecao-de-dependencia/java-injecaoDependencia/src/model/application/Program.java
@@ -1,13 +1,21 @@
 package model.application;
 
+import model.services.DeliverService;
 import model.services.PayService;
+import model.services.TaxService;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		PayService payService = new PayService();
+		// inversao de controle: agora o controle deixou de estar na propria classe e
+		// passou para o programa principal, gerando um baixo acoplamento e facilidade
+		// na manutencao do software
+		DeliverService deliverService = new DeliverService();
+		TaxService taxService = new TaxService();
+
+		PayService payService = new PayService(taxService, deliverService);
 
 		System.out.println("Resultado = " + payService.finalPrice(300.0, "SP"));
 	}
