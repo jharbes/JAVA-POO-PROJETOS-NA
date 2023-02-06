26ab8df9fe407543532f9762ff149ebb26c86603
Jorge Harbes <jharbes@hotmail.com>
2023-02-06 15:45:33 -0300

    layersPractice fim

--- XX-camadas-orm-dto/layersPractice/src/main/java/com/jharbes/layersPractice/services/UserService.java
+++ XX-camadas-orm-dto/layersPractice/src/main/java/com/jharbes/layersPractice/services/UserService.java
@@ -2,6 +2,7 @@ package com.jharbes.layersPractice.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jharbes.layersPractice.dto.UserDto;
 import com.jharbes.layersPractice.entities.User;
@@ -13,6 +14,7 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Transactional(readOnly = true)
 	public UserDto findById(Long id) {
 		User entity = userRepository.findById(id).get();
 		UserDto userDto = new UserDto(entity);
