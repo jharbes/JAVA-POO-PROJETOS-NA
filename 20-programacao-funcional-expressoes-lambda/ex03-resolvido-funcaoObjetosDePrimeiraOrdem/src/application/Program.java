76534a73669b66403199eec813a62ddc974c0c4b
Jorge Harbes <jharbes@hotmail.com>
2023-02-11 17:52:22 -0300

    Update Program.java

--- 20-programacao-funcional-expressoes-lambda/ex03-resolvido-funcaoObjetosDePrimeiraOrdem/src/application/Program.java
+++ 20-programacao-funcional-expressoes-lambda/ex03-resolvido-funcaoObjetosDePrimeiraOrdem/src/application/Program.java
@@ -6,7 +6,7 @@ import java.util.List;
 import entities.Product;
 
 public class Program {
-	
+
 	public static int compareProducts(Product p1, Product p2) {
 		return p1.getPrice().compareTo(p2.getPrice());
 	}
@@ -15,12 +15,16 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-		
+
+		// na programacao funcional podemos passar uma funcao como argumento de outra
+		// funcao, o que significa dizer que a funcao eh um objeto de primeira classe ou
+		// primeira ordem
 		list.sort(Program::compareProducts);
+
 		list.forEach(System.out::println);
 	}
 
