cae0cd95ef2eee5c3ba4f0712702033bf130f552
Jorge Harbes <jharbes@hotmail.com>
2023-02-12 11:06:42 -0300

    Function - exemplo com map - 4a versao: expressao lambda declarada

--- 20-programacao-funcional-expressoes-lambda/ex06-resolvido-function-exemploCom-map/src/application/Program.java
+++ 20-programacao-funcional-expressoes-lambda/ex06-resolvido-function-exemploCom-map/src/application/Program.java
@@ -2,11 +2,10 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -28,13 +27,20 @@ public class Program {
 		// exemplo primeiro convertemos de lista para stream para que assim possamos
 		// utilizar a funcao map
 		// 1a versao: implementacao da interface
-		// List<String> namesUpperCase = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
-		
+		// List<String> namesUpperCase = list.stream().map(new
+		// UpperCaseName()).collect(Collectors.toList());
+
 		// 2a versao: reference method com metodo estatico
-		// List<String> namesUpperCase = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
-		
+		// List<String> namesUpperCase =
+		// list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+
 		// 3a versao: reference method com metodo nao estatico
-		List<String> namesUpperCase = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		// List<String> namesUpperCase =
+		// list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+
+		// 4a versao: expressao lambda declarada
+		Function<Product, String> function = p -> p.getName().toUpperCase();
+		List<String> namesUpperCase = list.stream().map(function).collect(Collectors.toList());
 
 		// duas maneiras de imprimir a lista com forEach
 		namesUpperCase.forEach(p -> System.out.println(p));
