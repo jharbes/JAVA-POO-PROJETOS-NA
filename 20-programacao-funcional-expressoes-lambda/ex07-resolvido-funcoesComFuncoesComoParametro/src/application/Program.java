5fc97560be55b02c0ce34a1ca927e1fb65a5ab8a
Jorge Harbes <jharbes@hotmail.com>
2023-02-12 11:37:00 -0300

    ex07 resolvido Funcoes com Funcoes como parametros inicio

---
+++ 20-programacao-funcional-expressoes-lambda/ex07-resolvido-funcoesComFuncoesComoParametro/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		/*
+		 * Fazer um programa que, a partir de uma lista de produtos, calcule a soma dos
+		 * preços somente dos produtos cujo nome começa com "T".
+		 */
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+	}
+
+}
---
+++ 20-programacao-funcional-expressoes-lambda/ex07-resolvido-funcoesComFuncoesComoParametro/src/entities/Product.java
@@ -0,0 +1,68 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+
+	public Product() {
+	}
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	// metodo estatico trabalha com o produto que sera passado como argumento, logo
+	// deve receber um argumento "p" para que seja trabalhado
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+
+	// por ser um metodo nao estatico vai trabalhar com o proprio produto onde se
+	// encontra, por isso nao precisa de argumento, pois sera o proprio produto em
+	// si
+	public boolean nonStaticProductPredicate() {
+		return this.getPrice() >= 100.0;
+	}
+
+	// recebe o produto "p" por ser um metodo estatico, ou seja, trata com o
+	// parametro recebido e nao o proprio paramentro
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
+	// nao recebe argumento pois opera com o proprio produto
+	public void nonStaticPriceUpdate() {
+		this.setPrice(this.getPrice() * 1.1);
+	}
+	
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
+	}
+	
+	public String nonStaticUpperCaseName() {
+		return this.name.toUpperCase();
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + String.format("%.2f", price) + "]";
+	}
+}
