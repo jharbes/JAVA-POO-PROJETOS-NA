b2e06c53cbbfe99d19771e4d56ce9f7ccdc29d29
Jorge Harbes <jharbes@hotmail.com>
2023-02-19 10:06:41 -0300

    ex08 resolvido criando stream commit 02

--- 20-programacao-funcional-expressoes-lambda/ex08-resolvido-criacaoDeStreams/src/model/application/Program.java
+++ 20-programacao-funcional-expressoes-lambda/ex08-resolvido-criacaoDeStreams/src/model/application/Program.java
@@ -8,18 +8,25 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
-		Stream<Integer> st1 = list.stream();
-		System.out.println(Arrays.toString(st1.toArray()));
+		Stream<Integer> st1 = list.stream(); // transformando list em stream
 		
+		st1 = list.stream().map(x -> x * 10); // transformando list em stream e depois aplicando a funcao map, a funcao
+												// map aplica uma funcao a cada elemento da stream
+		
+		System.out.println(st1); // impressao sem formatacao
+		
+		System.out.println(Arrays.toString(st1.toArray())); // imprimindo a stream (a operacao toArray converte a stream
+															// em um vetor)
+
 		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
 		System.out.println(Arrays.toString(st2.toArray()));
-		
+
 		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
 		System.out.println(Arrays.toString(st3.limit(10).toArray()));
-		
-		Stream<Long> st4 = Stream.iterate(new long[]{ 0L, 1L }, p->new long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
+
+		Stream<Long> st4 = Stream.iterate(new long[] { 0L, 1L }, p -> new long[] { p[1], p[0] + p[1] }).map(p -> p[0]);
 		System.out.println(Arrays.toString(st4.limit(10).toArray()));
 	}
 
