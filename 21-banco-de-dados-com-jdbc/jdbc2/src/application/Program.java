cde0edc1cdaadaf2ef0d51d0ba7a9a7ffd9ae592
Jorge Harbes <jharbes@hotmail.com>
2023-02-24 14:57:49 -0300

    demo - recuperando dados do banco

--- 21-banco-de-dados-com-jdbc/jdbc2/src/application/Program.java
+++ 21-banco-de-dados-com-jdbc/jdbc2/src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import database.DB;
 
@@ -8,10 +11,28 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection connection = DB.getConnection();
+		Connection connection = null;
+
+		Statement statement = null; // onde preparamos a consulta a ser feita no banco
+
+		ResultSet resultSet = null; // onde guardaremos o resultado da consulta statement feita
 		
-		
-		DB.closeConnection();
+		try {
+			connection=DB.getConnection();
+			statement=connection.createStatement();
+			resultSet=statement.executeQuery("select * from department");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(resultSet);
+			DB.closeStatement(statement);
+			DB.closeConnection();
+		}
 	}
 
 }
