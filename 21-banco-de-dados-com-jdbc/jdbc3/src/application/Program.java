247cfcaa451aeec734a4c59941373f2e1e721481
Jorge Harbes <jharbes@hotmail.com>
2023-02-24 16:21:30 -0300

    inserindo os dados com retorno do id da tupla inserida

--- 21-banco-de-dados-com-jdbc/jdbc3/src/application/Program.java
+++ 21-banco-de-dados-com-jdbc/jdbc3/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -20,7 +22,8 @@ public class Program {
 		try {
 			connection = DB.getConnection();
 			preparedStatement = connection.prepareStatement(
-					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)");
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			preparedStatement.setString(1, "Carl Purple");
 			preparedStatement.setString(2, "carl@gmail.com");
 			preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
@@ -29,7 +32,15 @@ public class Program {
 
 			int rowsAffected = preparedStatement.executeUpdate();
 
-			System.out.println("Done! Rows Affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet resultSet = preparedStatement.getGeneratedKeys();
+				while (resultSet.next()) {
+					int id = resultSet.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			} else {
+				System.out.println("No rows affected!");
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
