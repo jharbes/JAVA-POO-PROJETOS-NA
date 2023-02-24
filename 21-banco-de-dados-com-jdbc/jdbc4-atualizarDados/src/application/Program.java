eb9c384a0bfb4aa05ea3cf838b40980c5c7e045b
Jorge Harbes <jharbes@hotmail.com>
2023-02-24 16:59:26 -0300

    jdbc4 atualizarDados fim

--- 21-banco-de-dados-com-jdbc/jdbc4-atualizarDados/src/application/Program.java
+++ 21-banco-de-dados-com-jdbc/jdbc4-atualizarDados/src/application/Program.java
@@ -1,10 +1,35 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import database.DB;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = DB.getConnection();
+
+			preparedStatement = connection
+					.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? WHERE (DepartmentId = ?)");
+			preparedStatement.setDouble(1, 200.0);
+			preparedStatement.setInt(2, 2);
+
+			int rowsAffected = preparedStatement.executeUpdate();
+
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(preparedStatement);
+			DB.closeConnection();
+		}
 	}
 
 }
