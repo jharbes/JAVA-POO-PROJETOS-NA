32ce8cf086385600c318c28610072084da1f56af
Jorge Harbes <jharbes@hotmail.com>
2023-02-24 16:48:23 -0300

    refatorando o nome dos projetos

--- 21-banco-de-dados-com-jdbc/jdbc1-codigoBase/src/database/DB.java
+++ 21-banco-de-dados-com-jdbc/jdbc1-codigoBase/src/database/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -43,4 +45,25 @@ public class DB {
 			throw new DbException(e.getMessage());
 		}
 	}
+
+	public static void closeStatement(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
+
